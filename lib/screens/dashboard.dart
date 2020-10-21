9621b39b8d6d1a0c0fa776cb375d6034faf68b50
RoboCOVA <tesfayeasmera@gmail.com>
2020-10-21 14:11:12 -0700

    added bacnk account information

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ligmone/wallet/pages/bankAccount.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Dashboard extends StatelessWidget {
@@ -49,52 +50,103 @@ class Dashboard extends StatelessWidget {
                 //     (MediaQuery.of(context).size.height / 2.35)
               ),
               itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => LoanCharts('Hello there'),
-                    //   ),
-                    // );
-                  },
-                  child: Container(
-                    margin: EdgeInsets.all(4),
-                    padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          spreadRadius: 2,
-                          blurRadius: 2,
+                if (index == 0) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BankAccountPage(),
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(
-                          height: 20,
-                        ),
-                        // Image.asset(images[index], height: 40, width: 40),
-                        Icon(icons[index], color: Colors.deepOrange, size: 32),
-
-                        Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Text(
-                            services[index],
-                            style: TextStyle(
-                                fontSize: 16,
-                                height: 1.2,
-                                fontWeight: FontWeight.normal),
-                            textAlign: TextAlign.center,
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
                           ),
-                        )
-                      ],
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20,
+                          ),
+                          // Image.asset(images[index], height: 40, width: 40),
+                          Icon(icons[index],
+                              color: Colors.deepOrange, size: 32),
+
+                          Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text(
+                              services[index],
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.2,
+                                  fontWeight: FontWeight.normal),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
+                  );
+                } else {
+                  return GestureDetector(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => LoanCharts('Hello there'),
+                      //   ),
+                      // );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20,
+                          ),
+                          // Image.asset(images[index], height: 40, width: 40),
+                          Icon(icons[index],
+                              color: Colors.deepOrange, size: 32),
+
+                          Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text(
+                              services[index],
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.2,
+                                  fontWeight: FontWeight.normal),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
               })),
     );
   }
--- lib/splash/components/body.dart
+++ lib/splash/components/body.dart
@@ -66,7 +66,7 @@ class _BodyState extends State<Body> {
                     ),
                     Spacer(flex: 3),
                     DefaultButton(
-                      text: "Continue",
+                      text: "Get Started",
                       // press: () {
                       //   Navigator.push(
                       //     context,
